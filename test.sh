61232e5c3b2d3e9aac83ede18840a2fe9e1b4a30
yiren1105 <35779946+yiren1105@users.noreply.github.com>
2021-04-13 11:34:41 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,8 @@
 # Update version in version.sbt depends on libs-snapshot-local or libs-release-local
 version_file="version.sbt"
 
-function lazygit() {
-    git add .
-    git commit -a -m "$1"
-    git push
-}
-
 sed -i -e 's/-SNAPSHOT//g' $version_file
 
-lazygit "Creating release version"
+git commit .
+git commit -m "Create release version"
+git push origin master
